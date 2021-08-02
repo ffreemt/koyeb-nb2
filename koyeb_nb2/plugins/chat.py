76adbfe6cdff6a814c5538613378acb6a3186ee3
freemt <yucongo+fmt@gmail.com>
2021-08-02 15:50:51 +0800

    Update chat plugin

--- koyeb_nb2/plugins/chat.py
+++ koyeb_nb2/plugins/chat.py
@@ -1,7 +1,11 @@
 """Chat away to your heart's content."""
+# pylint: disable=invalid-name
 # import pdir
+from random import choice
+import jsonpath_ng
 import logzero
 from logzero import logger
+from fastlid import fastlid
 
 import nonebot
 
@@ -11,36 +15,26 @@ from nonebot.adapters.cqhttp import Bot, Event
 
 # from nonebot.log import logger
 
+from koyeb_nb2.fetch_convbot import fetch_convbot
+
 logzero.loglevel(10)
 
 # test = on_message()
 # test = nonebot.on_command("chat", aliases={"xianliao", "闲聊"}, priority=5,)
-test = nonebot.on_message(priority=5, block=False)
+
+on_message = nonebot.on_message(priority=5)
 
 
-@test.handle()
+@on_message.handle()
 async def handle(bot: Bot, event: Event, state: dict):
     """Handle messages.
-     
-    # not tome
-    if not event.is_tome():
-        return
-    
-    # all [None, None] 
-    if not any([elm.value for elm in 
-    jsonpath_ng.parse("$..command").find(state)]):
-        return 
-    
-    # collect user msg
-    msg = event.get_plaintext()
-    resp = bot_response(msg)
-    
-    # logic for checking consecutive empty resp 
+
+    # logic for checking consecutive empty resp
     if resp.strip():
         ...
     else:
         ... # append extra msg
-    
+
     """
     _ = """
     logger.debug(pdir(event))
@@ -91,3 +85,72 @@ async def handle(bot: Bot, event: Event, state: dict):
     [elm.full_path for elm in jsonpath_ng.parse("$..command").find(r)]
 
     """
+    # not tome
+    if not event.is_tome():
+        return
+
+    # all [None, None]
+    logger.debug("[elm.value for elm in jsonpath_ng.parse('$..command').find(state)]: %s", [elm.value for elm in jsonpath_ng.parse("$..command").find(state)])
+    logger.debug("not any([elm.value for elm in jsonpath_ng.parse('$..command').find(state)]): %s", not any([elm.value for elm in jsonpath_ng.parse("$..command").find(state)]))
+
+    if any([elm.value for elm in jsonpath_ng.parse("$..command").find(state)]):  # _prefix or _suffix command, ignore
+        return
+
+    # [None, None]: deemed as chat
+    # initialzie attr for recoding consecutive empty responses
+    # and prev_resp
+    try:
+        handle.c_resp
+        handle.prev_resp
+    except AttributeError:
+        handle.c_resp = 0
+        handle.prev_resp = ""
+
+    # process messages
+    msg = event.get_plaintext()
+
+    # detect language
+    lang = fastlid(msg)
+    if lang[0] not in ["en"]:
+        try:
+            await bot.send(message=f"I detect you are talking in [{lang}], which I currently are unable to understand. (I'll be able to chat in German and French soon.)", event=event)
+        except Exception as e:
+            logger.error(e)
+
+    try:
+        # resp = bot_response(msg)
+        resp = await fetch_convbot(msg, handle.prev_resp)
+        handle.prev_resp = resp
+    except Exception as e:
+        logger.error(e)
+        resp = str(e)
+
+    # empty response from the bot
+    if not resp.strip():
+        resp = "..."
+        handle.c_resp += 1
+    else:  # reset counter
+        handle.c_resp = 0
+
+    if handle.c_resp > 2:
+        msg = choice([
+            "You are talking nonesense or something is probably wrong. In the latter case, conatct my master.",
+            "You are talking nonesense or something is probably wrong. In the latter case, contact my master. In the former case, well...",
+            "Eitehr you are talking nonesense or something is probably wrong. In the latter case, contact my master.",
+            "If you are not talking nonesense, then something is probably wrong.",
+        ])
+        try:
+            await bot.send(
+                message=f"...({msg})",
+                event=event,
+            )
+            handle.c_resp = 0
+        except Exception as e:
+            logger.error(e)
+
+        return
+
+    try:
+        await bot.send(message=resp, event=event)
+    except Exception as e:
+        logger.error(e)
