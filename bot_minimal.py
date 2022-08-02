2e50ca413553339cdbe2bfdf10dd0574ce93f8db
freemt <yucongo+fmt@gmail.com>
2022-08-02 18:03:52 +0800

    Update ping1 ping2

--- bot_minimal.py
+++ bot_minimal.py
@@ -10,6 +10,7 @@ from nonebot.adapters.onebot.v11 import (
     MessageSegment,
     unescape,
 )
+from nonebot.params import Command, CommandArg, RawCommand
 from nonebot.plugin import on_command
 from nonebot.rule import to_me
 
@@ -26,16 +27,25 @@ node = platform.node()
 # override built-in echo
 # @echo.handle()
 
-ping = on_command("ping", aliases={"p",}, priority=1, block=False)
+ping1 = on_command("ping1", aliases={"p1",}, priority=1, block=False)
 
 
-@ping.handle()
-async def handle(bot: Bot, event: MessageEvent):
+@ping1.handle()
+async def handle1(bot: Bot, event: MessageEvent):
     try:
-        await bot.send(message=f"{node}: {event.get_message()}", event=event)
+        await bot.send(message=f"-{node}: {event.get_message()}", event=event)
     except Exception as e:
         logger.error(e)
 
+ping2 = on_command("ping2", aliases={"p2",}, priority=1, block=False)
+
+@ping2.handle()
+async def handle(message: Message = CommandArg()):
+    try:
+        _ = f"{node}{': ' if str(message).strip() else ''}{message}"
+        await ping2.send(message=_)
+    except Exception as e:
+        logger.error(e)
 
 nonebot.load_plugin("koyeb_nb2.plugins.nb2chan")
 # nonebot.load_plugin("koyeb_nb2.plugins.autohelp")
