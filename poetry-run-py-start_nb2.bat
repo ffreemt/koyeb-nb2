f582a9c5c9205dbab6b8a413492f466280683b59
ffreemt <yucongo+fmt@gmail.com>
2022-10-15 13:09:41 +0800

    Update poetry-run-py-start_nb2.bat

---
+++ poetry-run-py-start_nb2.bat
@@ -0,0 +1,4 @@
+poetry run python start_nb2.py
+
+echo to test: curl "127.0.0.1:8680/nb2chan/?qq=41947782&msg=hello" -H "token: DEMO_TOKEN"
+echo run poetry export --without-hashes -o requirements.txt before deploying to koyeb
